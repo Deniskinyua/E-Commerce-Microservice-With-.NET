c5d893a7b63d48cb99eb8f491c9291124893cfe1
DenisKinyua <kimenjudenis@gmail.com>
2025-05-03 15:16:46 +0300

    Implementing common/global exception handler

--- Ecommerce.SharedLibrarySln/CommonSharedLibrary/Response/Response.cs
+++ Ecommerce.SharedLibrarySln/CommonSharedLibrary/Response/Response.cs
@@ -1,3 +1,5 @@
-namespace CommonSharedLibrary.Response;
+namespace CommonSharedLibrary.Responses
+{
+    public record Response( bool Flag = false, string Message= null!);
+}
 
-public record Response( bool Flag = false, string Message= null!);
